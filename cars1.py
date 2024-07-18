bcd616a250d954711c4be6c4e6e11babaac8027d
Asadbek234 <sobirovasadbek094@gmail.com>
2024-07-18 21:29:42 +0500

    Update cars1.py
    add first commit

--- cars1.py
+++ cars1.py
@@ -1,1 +1,10 @@
+class Phone:
+  def _init_(self , model,color;str,price;int) :
+    self.model=model
+    self.color=color
+    self.price=price
+def __str__(self)
+  return f"model:{self.model} \n color{self.color} \n price {self.price}"
 
+phones=Phone('samsung S 24 ULTRA ', 'black', '25.0000$')
+  
